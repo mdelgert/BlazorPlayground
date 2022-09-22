825ab18889adf930a51e6be4a22e10b6d7d15596
melgert <mdelgert@yahoo.com>
2022-09-21 20:27:22 -0400

    Add modified source

---
+++ BlazorPlayGround2/BlazorPlayGround.Data/BlazorPlayGroundContext.cs
@@ -0,0 +1,16 @@
+﻿using BlazorPlayGround.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorPlayGround.Data
+{
+    public class BlazorPlayGroundContext : DbContext
+    {
+        public BlazorPlayGroundContext(DbContextOptions<BlazorPlayGroundContext> options) : base(options)
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        public DbSet<UserMasterModel> UserMaster { get; set; }
+    }
+}
